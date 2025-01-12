7c22ee73f6258eec137bf365a6852cf579e7a14d
vibimvab <vibimvab@gmail.com>
2025-01-12 17:52:16 -0500

    1/5/2025

--- LeetCode/11.py
+++ LeetCode/11.py
@@ -1,28 +1,56 @@
-def maxArea(heights: [int]) -> int:
-    # size = 0
-    size_list = []
-    length = len(heights)
+from typing import List
 
-    flag_start_height = -1
-    for start_pos, start_height in enumerate(heights):
-        if flag_start_height < start_height:
-            flag_start_height = start_height
-        else:
-            continue
 
-        flag_end_height = -1
-        for end_pos, end_height in enumerate(heights[-1:start_pos:-1]):
-            if end_height >= start_height:
-                size_list.append(start_height * (length - start_pos - end_pos - 1))
-                break
+class Solution:
+    def maxArea(self, heights: List[int]) -> int:
+        # return self.maxArea_bf(heights)
+        return self.maxArea_greedy(heights)
 
-            elif end_height > flag_end_height:
-                flag_end_height = end_height
-                size_list.append(end_height * (length - start_pos - end_pos - 1))
+    def maxArea_greedy(self, heights: List[int]) -> int:
+        n = len(heights)
+        left = 0
+        right = n-1
 
-    return max(size_list)
+        area = []
+        while left < right:
+            area.append((right - left) * min(heights[left], heights[right]))
+            if left < right:
+                left += 1
+            else:
+                right -= 1
+
+        return max(area)
+
+    def maxArea_bf(self, heights: List[int]) -> int:
+        # brute force solution
+        n = len(heights)
+        max_water = 0
+        left_prev = 0
+
+        for left, left_h in enumerate(heights):
+            if left_h <= left_prev:
+                continue
+
+            right_h = 0
+            water = [0]
+            for right in range(n - 1, left - 1, -1):
+                if right_h < heights[right]:
+                    right_h = heights[right]
+
+                    if right_h < left_h:
+                        water.append((right - left) * right_h)
+                    else:
+                        water.append((right - left) * left_h)
+                        break
+
+            max_water = max(max_water, max(water))
+
+        return max_water
 
 
 if __name__ == "__main__":
-    print(maxArea([1,8,6,2,5,4,8,3,7]))
-    print(maxArea(range(10000)+range(9999, 0, -1)))
+    s = Solution()
+    print(s.maxArea([1, 8, 6, 2, 5, 4, 8, 3, 7]))
+    print(s.maxArea(list(range(10000)) + list(range(9999, 0, -1))))
+    print(s.maxArea([2, 0]))
+    print(s.maxArea([1, 1]))
